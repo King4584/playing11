b663ec25cd46bf7fcf6d2c03c591bd534f897433
Utsav <pk0385599@gmail.com>
2024-08-21 16:01:09 +0530

    Added input field

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ function App() {
 
   return (
     <>
-      <div className='flex items-center justify-center h-screen bg-black'> 
+      <div className='flex items-center justify-center h-screen '> 
         <div className='text-white font-serif'>
-          
+          <input className='m-2 p-2 border-t-white rounded-2xl' type='text' placeholder='Enter player name' />
+          <button className='m-2 p-2 bg-slate-500 rounded-2xl' type='submit'>Submit</button>
         </div>
       </div>
     </>
