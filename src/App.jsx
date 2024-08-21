4253a5066f6c38e82f2af5f7dc55d3750d0404c0
Utsav <pk0385599@gmail.com>
2024-08-21 15:57:11 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex items-center justify-center h-screen bg-black'> 
+        <div className='text-white font-serif'>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
